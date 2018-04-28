af5c9dc64c96490ce35dad7a79c15d1350f54008
jorge <jorge mauro goncalves>
2018-04-28 14:39:35 -0300

    adicionado a classe que vai escutar os outros clientes

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,26 @@
+import java.io.IOException;
+
+// classe principal do cliente
 public class Client implements Runnable {
 
     public static Client[] otheClients;
     public int id;
+    public Listener listener;
+    public Client(int id){
+        this.id=id;
+        try {
+            this.listener=new Listener(this);
+            new Thread(listener).start();
+            new Thread(this).start();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void run() {
+        while(true){
 
+        }
     }
 }
--- src/Listener.java
+++ src/Listener.java
@@ -1,14 +1,14 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+// classe que vai escutar os outros clientes que vai
 public class Listener  implements Runnable{
 
     ServerSocket server;
 
     public Listener(Client other) throws IOException {
         try {
-            server=new ServerSocket(5000+other.id);
+            server=new ServerSocket(500+other.id);
         } catch (IOException e) {
             e.printStackTrace();
         }
